b459aa8a0156ac68c8ccda4321a16758d2163995
Chad Linthicum <portableanswers@gmail.com>
2021-11-14 08:54:51 -0500

    Added computerChoice variable locally and it fixed the button repeat bug

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 const rock = 0;
 const paper = 1;
 const scissors = 2;
-let computerChoice = Math.floor(Math.random() * 3);
+// let computerChoice = Math.floor(Math.random() * 3);
 let wins = 0;
 let losses = 0;
 let ties = 0;
 
+// document.addEventListener("click", event => {
+//   event.preventDefault();
+//   alert('woah');
+// })
+
 function chooseRock() {
+  let computerChoice = Math.floor(Math.random() * 3);
   if (computerChoice==scissors) {
     wins++;
     document.getElementById('wins').innerText = "Wins: " + wins;
     document.getElementById('userChoice').innerText = "You chose rock";
     document.getElementById('computerChoice').innerText = "Computer chose scissors";
     
-    var img1 = new Image();
-    img1.src = "/assets/theRockSlam.gif";
-    document.getElementById("winSlam").appendChild(img1);
+    // var img1 = new Image();
+    // img1.src = "/assets/theRockSlam.gif";
+    // document.getElementById("winSlam").appendChild(img1);
     
   } else if (computerChoice==paper) {
     losses++;
@@ -31,15 +37,17 @@ function chooseRock() {
 }
 
 function choosePaper() {
+  let computerChoice = Math.floor(Math.random() * 3);
   if (computerChoice==rock) {
     wins++;
     document.getElementById('wins').innerText = "Wins: " + wins;
     document.getElementById('userChoice').innerText = "You chose paper";
     document.getElementById('computerChoice').innerText = "Computer chose rock";
 
-    var img = new Image();
-    img.src = "/assets/paperBoy.gif";
-    document.getElementById("winSlam").appendChild(img);
+    // var img = new Image();
+    // img.src = "/assets/paperBoy.gif";
+    // document.getElementById("winSlam").appendChild(img);
+
   } else if (computerChoice==scissors) {
     losses++;
     document.getElementById('losses').innerText = "Losses: " + losses;
@@ -54,15 +62,17 @@ function choosePaper() {
 }
 
 function chooseScissors() {
+  let computerChoice = Math.floor(Math.random() * 3);
   if (computerChoice==paper) {
     wins++;
     document.getElementById('wins').innerText = "Wins: " + wins;
     document.getElementById('userChoice').innerText = "You chose scissors";
     document.getElementById('computerChoice').innerText = "Computer chose paper";
 
-    var img = new Image();
-    img.src = "/assets/edward.gif";
-    document.getElementById("winSlam").appendChild(img);
+    // var img = new Image();
+    // img.src = "/assets/edward.gif";
+    // document.getElementById("winSlam").appendChild(img);
+
   } else if (computerChoice==rock) {
     losses++;
     document.getElementById('losses').innerText = "Losses: " + losses;
