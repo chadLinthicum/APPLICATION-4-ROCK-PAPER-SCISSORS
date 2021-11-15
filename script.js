80cd3ca734896c59eb63e2266e56309a8fb40fc3
Chad Linthicum <portableanswers@gmail.com>
2021-11-15 12:38:11 -0500

    Done! ul center could be improved but fine for now.

--- script.js
+++ script.js
@@ -5,34 +5,49 @@ let wins = 0;
 let losses = 0;
 let ties = 0;
 
-
-
 function chooseRock() {
   let computerChoice = Math.floor(Math.random() * 3);
   if (computerChoice==scissors) {
     wins++;
     document.getElementById('wins').innerText = "Wins: " + wins;
-    document.getElementById('userChoice').innerText = "You chose rock";
-    document.getElementById('computerChoice').innerText = "Computer chose scissors";
+    document.getElementById('userChoice').innerText = "... You chose rock ...";
+    document.getElementById('computerChoice').innerText = "... Computer chose scissors ...";
     
     var img = new Image();
     img.src = "/assets/theRockSlam.gif";
-    var winGIF = document.getElementById("winGIF");
-    if (winGIF.childNodes=img) {
-      winGIF.removeChild(winGIF.lastElementChild);
-      winGIF.appendChild(img);
+    var gameResult = document.getElementById("gameResult");
+    if (gameResult.childNodes=img) {
+      gameResult.removeChild(gameResult.lastElementChild);
+      gameResult.appendChild(img);
     }    
     
   } else if (computerChoice==paper) {
     losses++;
     document.getElementById('losses').innerText = "Losses: " + losses;
-    document.getElementById('userChoice').innerText = "You chose rock";
-    document.getElementById('computerChoice').innerText = "Computer chose paper";
+    document.getElementById('userChoice').innerText = "... You chose rock ...";
+    document.getElementById('computerChoice').innerText = "... Computer chose paper ...";
+
+    var img = new Image();
+    img.src = "/assets/youLose.PNG";
+    var gameResult = document.getElementById("gameResult");
+    if (gameResult.childNodes=img) {
+      gameResult.removeChild(gameResult.lastElementChild);
+      gameResult.appendChild(img);
+    }
+
   } else if (computerChoice==rock) {
     ties++;
     document.getElementById('ties').innerText = "Ties: " + ties;
-    document.getElementById('userChoice').innerText = "You chose rock";
-    document.getElementById('computerChoice').innerText = "Computer chose rock as well...";
+    document.getElementById('userChoice').innerText = "... You chose rock ...";
+    document.getElementById('computerChoice').innerText = "... Computer chose rock as well ...";
+
+    var img = new Image();
+    img.src = "/assets/draw.PNG";
+    var gameResult = document.getElementById("gameResult");
+    if (gameResult.childNodes=img) {
+      gameResult.removeChild(gameResult.lastElementChild);
+      gameResult.appendChild(img);
+    }
   }
 }
 
@@ -41,29 +56,45 @@ function choosePaper() {
   if (computerChoice==rock) {
     wins++;
     document.getElementById('wins').innerText = "Wins: " + wins;
-    document.getElementById('userChoice').innerText = "You chose paper";
-    document.getElementById('computerChoice').innerText = "Computer chose rock";
+    document.getElementById('userChoice').innerText = "... You chose paper ...";
+    document.getElementById('computerChoice').innerText = "... Computer chose rock ...";
 
     var img = new Image();
     img.src = "/assets/paperBoy.gif";
-    var winGIF = document.getElementById("winGIF");
-    if (winGIF.childNodes=img) {
-      winGIF.removeChild(winGIF.lastElementChild);
-      winGIF.appendChild(img);
+    var gameResult = document.getElementById("gameResult");
+    if (gameResult.childNodes=img) {
+      gameResult.removeChild(gameResult.lastElementChild);
+      gameResult.appendChild(img);
       img.className = "GIFs";
     }   
-    
-
+  
   } else if (computerChoice==scissors) {
     losses++;
     document.getElementById('losses').innerText = "Losses: " + losses;
-    document.getElementById('userChoice').innerText = "You chose paper";
-    document.getElementById('computerChoice').innerText = "Computer chose scissors";
+    document.getElementById('userChoice').innerText = "... You chose paper ...";
+    document.getElementById('computerChoice').innerText = "... Computer chose scissors ...";
+
+    var img = new Image();
+    img.src = "/assets/youLose.PNG";
+    var gameResult = document.getElementById("gameResult");
+    if (gameResult.childNodes=img) {
+      gameResult.removeChild(gameResult.lastElementChild);
+      gameResult.appendChild(img);
+    }
+
   } else if (computerChoice==paper) {
     ties++;
     document.getElementById('ties').innerText = "Ties: " + ties;
-    document.getElementById('userChoice').innerText = "You chose paper";
-    document.getElementById('computerChoice').innerText = "Computer chose paper as well...";
+    document.getElementById('userChoice').innerText = "... You chose paper ...";
+    document.getElementById('computerChoice').innerText = "... Computer chose paper as well ...";
+
+    var img = new Image();
+    img.src = "/assets/draw.PNG";
+    var gameResult = document.getElementById("gameResult");
+    if (gameResult.childNodes=img) {
+      gameResult.removeChild(gameResult.lastElementChild);
+      gameResult.appendChild(img);
+    }
   }
 }
 
@@ -72,27 +103,44 @@ function chooseScissors() {
   if (computerChoice==paper) {
     wins++;
     document.getElementById('wins').innerText = "Wins: " + wins;
-    document.getElementById('userChoice').innerText = "You chose scissors";
-    document.getElementById('computerChoice').innerText = "Computer chose paper";
+    document.getElementById('userChoice').innerText = "... You chose scissors ...";
+    document.getElementById('computerChoice').innerText = "... Computer chose paper ...";
 
     var img = new Image();
     img.src = "/assets/edward.gif";
-    var winGIF = document.getElementById("winGIF");
-    if (winGIF.childNodes=img) {
-      winGIF.removeChild(winGIF.lastElementChild);
-      winGIF.appendChild(img);
+    var gameResult = document.getElementById("gameResult");
+    if (gameResult.childNodes=img) {
+      gameResult.removeChild(gameResult.lastElementChild);
+      gameResult.appendChild(img);
     }   
 
   } else if (computerChoice==rock) {
     losses++;
     document.getElementById('losses').innerText = "Losses: " + losses;
-    document.getElementById('userChoice').innerText = "You chose scissors";
-    document.getElementById('computerChoice').innerText = "Computer chose rock";
+    document.getElementById('userChoice').innerText = "... You chose scissors ...";
+    document.getElementById('computerChoice').innerText = "... Computer chose rock ...";
+
+    var img = new Image();
+    img.src = "/assets/youLose.PNG";
+    var gameResult = document.getElementById("gameResult");
+    if (gameResult.childNodes=img) {
+      gameResult.removeChild(gameResult.lastElementChild);
+      gameResult.appendChild(img);
+    }
+
   } else if (computerChoice==scissors) {
     ties++;
     document.getElementById('ties').innerText = "Ties: " + ties;
-    document.getElementById('userChoice').innerText = "You chose scissors";
-    document.getElementById('computerChoice').innerText = "Computer chose scissors as well...";
+    document.getElementById('userChoice').innerText = "... You chose scissors ...";
+    document.getElementById('computerChoice').innerText = "... Computer chose scissors as well ...";
+
+    var img = new Image();
+    img.src = "/assets/draw.PNG";
+    var gameResult = document.getElementById("gameResult");
+    if (gameResult.childNodes=img) {
+      gameResult.removeChild(gameResult.lastElementChild);
+      gameResult.appendChild(img);
+    }
   }
 }
 
